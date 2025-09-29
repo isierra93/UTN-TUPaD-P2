7398e30fc4e2697078de28384d86e6996a873836
Ivan Sierra <sierraivan93@gmail.com>
2025-09-29 08:12:26 -0300

    ejercicios 6

---
+++ 06 - Colecciones/CasoPractico2/src/Biblioteca.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private String nombre;
+    private List<Libro> libros = new ArrayList<Libro>();
+
+    public Biblioteca(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarLibro(String isbn, String titulo, int anioPublicacion, Autor autor) {
+        Libro libro = new Libro(isbn, titulo,anioPublicacion, autor);
+        libros.add(libro);
+    }
+
+    public void listarLibros() {
+        for (Libro l : libros ) {
+            System.out.println(l);
+        }
+    }
+
+    public Libro buscarLibroPorIsbn(String isbn) {
+        Libro libroEncontrado = null;
+        int i = 0;
+        while (i < libros.size() && !libros.get(i).getIsbn().equals(isbn)){
+            i++;
+        }
+        if (i < libros.size()){
+            libroEncontrado = libros.get(i);
+        }
+        return libroEncontrado;
+    }
+
+    public void eliminarLibro(String isbn) {
+        Libro libroaEliminar = buscarLibroPorIsbn(isbn);
+        if (libroaEliminar != null) {
+            libros.remove(libroaEliminar);
+        }
+    }
+
+    public int obtenerCantidadLibros() {
+        return libros.size();
+    }
+
+    public List<Libro> filtrarLibrosPorAnio(int anio) {
+        List<Libro> librosFiltradosPorAnio = new ArrayList<>();
+        for (Libro l : libros){
+            if (l.getAnioPublicacion() == anio) {
+                librosFiltradosPorAnio.add(l);
+            }
+        }
+        return librosFiltradosPorAnio;
+    }
+
+    public List<Autor> mostrarAutoresDisponibles() {
+        List<Autor> autoresDisponibles = new ArrayList<>();
+        for (Libro l : libros) {
+            if (autoresDisponibles.get){
+
+            }
+            autoresDisponibles.add(l.getAutor());
+        }
+        return autoresDisponibles;
+    }
+
+    public Autor buscarAutorPorId(String id) {
+        Autor autorEncontrado = null;
+        int i = 0;
+        while (i < libros.size() && !libros.get(i).getIsbn().equals(isbn)){
+            i++;
+        }
+        if (i < libros.size()){
+            autorEncontrado = libros.get(i);
+        }
+        return autorEncontrado;
+    }
+
+
+}
--- 06 - Colecciones/CasoPractico2/src/Main.java
+++ 06 - Colecciones/CasoPractico2/src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
+//1. Creamos una biblioteca.
+//2. Crear al menos tres autores
+//3. Agregar 5 libros asociados a alguno de los Autores a la biblioteca.
+//4. Listar todos los libros con su información y la del autor.
+//5. Buscar un libro por su ISBN y mostrar su información.
+//6. Filtrar y mostrar los libros publicados en un año específico.
+//7. Eliminar un libro por su ISBN y listar los libros restantes.
+//8. Mostrar la cantidad total de libros en la biblioteca.
+//9. Listar todos los autores de los libros disponibles en la biblioteca
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Creamos una biblioteca.
+        Biblioteca b = new Biblioteca("Biblioteca Nacional Argentina");
+        //Creamos los autores
+        Autor a1 = new Autor("1", "Stephen King", "Estados Unidos");
+        Autor a2 = new Autor("2", "Agustina Bazterrica", "Argentina");
+        Autor a3 = new Autor("3", "Aldous Huxley", "Inglaterra");
+        //Agregamos los libros a la biblioteca
+        b.agregarLibro("89UASD", "La larga marcha", 1979, a1);
+        b.agregarLibro("NLKSAD", "Cadáver exquisito", 2017, a2);
+        b.agregarLibro("N1ID2U", "Un mundo feliz", 1932, a3);
+        b.agregarLibro("OIJ983", "It", 1986, a1);
+        b.agregarLibro("POK098", "El resplandor", 1977, a1);
+        //Listar los libros
+        System.out.println("Listado de libros:");
+        b.listarLibros();
+        //Buscar un libro por su isbn
+        System.out.println("Busqueda de libro ISBN N1ID2U:");
+        b.buscarLibroPorIsbn("N1ID2U").mostrarInfo();
+        //
+        List<Autor> autorList = b.mostrarAutoresDisponibles();
+        for (Autor a : autorList) {
+            System.out.println(a);
         }
     }
+
 }
