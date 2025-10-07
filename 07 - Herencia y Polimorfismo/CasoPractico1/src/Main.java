fc24480cc2d23e9b2c93c227026f11f0b3f2804e
Ivan Sierra <sierraivan93@gmail.com>
2025-10-07 20:44:22 -0300

    Ejercicio 7.1

--- 07 - Herencia y Polimorfismo/CasoPractico1/src/Main.java
+++ 07 - Herencia y Polimorfismo/CasoPractico1/src/Main.java
@@ -1,11 +1,14 @@
 
+//● Clase base: Vehículo con atributos marca, modelo y metodo mostrarInfo()
+//● Subclase: Auto con atributo adicional cantidadPuertas, sobrescribe
+//mostrarInfo()
+//● Tarea: Instanciar un auto y mostrar su información completa.
+
 public class Main {
     public static void main(String[] args) {
 
     Vehiculo a = new Auto("Toyota", "Corolla", 4);
     a.mostrarInfo();
-
-
 
     }
 }
